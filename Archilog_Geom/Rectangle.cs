fd6dee8d75ad5fd2309b6bc30142cae6d1c76154
mmayolini <maxime.mayolini@gmail.com>
2018-04-16 14:30:31 +0200

    added composite

---
+++ Archilog_Geom/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archilog_Geom
+{
+    class Rectangle : SingleShape
+    {
+    }
+}
