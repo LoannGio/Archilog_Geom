c3c95aeecc4f34ac3a7129409af0ec4bb9a65432
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2018-04-16 18:22:10 +0200

    draw in toolbar

--- Archilog_Geom/View/CsGraphics.cs
+++ Archilog_Geom/View/CsGraphics.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Windows.Forms;
 
@@ -9,7 +10,7 @@ namespace Archilog_Geom
 {
     public class CsGraphics : Form, IObservableGraphics
     {
-        private Panel toolBarPanel;
+        private Panel _toolBarPanel;
         private Panel _drawingPanel;
 
         public CsGraphics()
@@ -21,8 +22,8 @@ namespace Archilog_Geom
 
         public void InitializeToolBar()
         {
-            var shapes = Mediator.Instance.ToolBar.ToolBarShapes;
-            for (int i = 0; i < shapes.Count; i++)
+            var shapesCount = Mediator.Instance.ToolBar.ToolBarShapes.Count;
+            for (int i = 0; i < shapesCount; i++)
             {
                 #region create the sub panel
                 Panel p = new Panel();
@@ -30,25 +31,28 @@ namespace Archilog_Geom
                 int itemXposition = 0;
                 p.BorderStyle = BorderStyle.FixedSingle;
                 p.Location = new Point(itemXposition, i*itemHeight);
-                p.Size = new Size(toolBarPanel.Width, itemHeight);
+                p.Size = new Size(_toolBarPanel.Width, itemHeight);
                 #endregion
 
-                #region draw shape on the sub panel
+                #region draw shape on sub panel
+                Image img = new Bitmap(p.Width, p.Height);
+                Graphics g = Graphics.FromImage(img);
 
-                Graphics g = p.CreateGraphics();
+                
+
                 g.DrawRectangle(new Pen(Color.Blue), 10,10,50,50);
+                p.BackgroundImage = img;
                 #endregion
 
-                toolBarPanel.Controls.Add(p);
-                //toolBarPanel.Refresh();
+                _toolBarPanel.Controls.Add(p);
             }
-
         }
+
 
         private void InitializeComponent()
         {
             this._drawingPanel = new System.Windows.Forms.Panel();
-            this.toolBarPanel = new System.Windows.Forms.Panel();
+            this._toolBarPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // _drawingPanel
@@ -64,18 +68,18 @@ namespace Archilog_Geom
             // 
             // toolBarPanel
             // 
-            this.toolBarPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this._toolBarPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.toolBarPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.toolBarPanel.Location = new System.Drawing.Point(13, 31);
-            this.toolBarPanel.Name = "toolBarPanel";
-            this.toolBarPanel.Size = new System.Drawing.Size(134, 459);
-            this.toolBarPanel.TabIndex = 1;
+            this._toolBarPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._toolBarPanel.Location = new System.Drawing.Point(13, 31);
+            this._toolBarPanel.Name = "_toolBarPanel";
+            this._toolBarPanel.Size = new System.Drawing.Size(134, 459);
+            this._toolBarPanel.TabIndex = 1;
             // 
             // CsGraphics
             // 
             this.ClientSize = new System.Drawing.Size(836, 493);
-            this.Controls.Add(this.toolBarPanel);
+            this.Controls.Add(this._toolBarPanel);
             this.Controls.Add(this._drawingPanel);
             this.Name = "CsGraphics";
             this.Text = "Archilog Geom";
