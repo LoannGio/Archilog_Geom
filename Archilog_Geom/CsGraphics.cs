a13885849c1bb96a13300c3c08d8f35b0c519a1e
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2018-04-16 14:16:58 +0200

    test

---
+++ Archilog_Geom/CsGraphics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Archilog_Geom
+{
+    public class CsGraphics : Form, ObservableGraphics
+    {
+        public CsGraphics()
+        {
+            Label l = new Label();
+            l.Text = "toto";
+            l.Show();
+            l.Location = new Point(13, 13);
+            l.AutoSize = true;
+            this.Controls.Add(l);
+
+        }
+    }
+}
--- Archilog_Geom/Mediator.cs
+++ Archilog_Geom/Mediator.cs
@@ -6,17 +6,14 @@ using System.Windows.Forms;
 
 namespace Archilog_Geom
 {
-    static class Program
+    static class Mediator
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CsGraphics());
         }
     }
 }
